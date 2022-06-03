ea768cc26f46bb352e784aa1a88837f6f538e34b
Tabuci Octavian <octavian@tabuci.org>
2022-06-03 09:58:10 +0300

    fix

--- core/clients/db/scope/scope.go
+++ core/clients/db/scope/scope.go
@@ -1,9 +1,15 @@
 package scope
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 func IsDeleted(db *gorm.DB) *gorm.DB {
 	return db.Where("is_deleted = ?", true)
+}
+
+func IsDeletedAt(db *gorm.DB) *gorm.DB {
+	return db.Where("deleted_at IS NOT NULL")
 }
 
 func IsDeletedT(tableName string) func(db *gorm.DB) *gorm.DB {
@@ -12,13 +18,45 @@ func IsDeletedT(tableName string) func(db *gorm.DB) *gorm.DB {
 	}
 }
 
+func IsDeletedAtT(tableName string) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Where(tableName + ".deleted_at IS NOT NULL")
+	}
+}
+
 func IsNotDeleted(db *gorm.DB) *gorm.DB {
+	// Migrator has column queries the DB information schema for existence,
+	// but this is slow in performance, that's why, we will check the struct itself (the provided model)
+	/*if db.Migrator().HasColumn(db.Statement.Model, "IsDeleted") {
+		return db.Where("is_deleted = ?", false)
+	} else if db.Migrator().HasColumn(db.Statement.Model, "DeletedAt") {
+		return db.Where("deleted_at IS NULL")
+	}*/
+
+	// din pacate db.Statement.Model e nil... ceea ce inseamna ca nu putem sti despre ce model/tabel e vorba
+	/*model := _struct.New(db.Statement.Model)
+	if model.FieldExists("IsDeleted") {
+		return db.Where("is_deleted = ?", false)
+	} else if model.FieldExists("DeletedAt") {
+		return db.Where("deleted_at IS NULL")
+	}
+	return db
+	*/
 	return db.Where("is_deleted = ?", false)
+}
+func IsNotDeletedAt(db *gorm.DB) *gorm.DB {
+	return db.Where("deleted_at IS NULL")
 }
 
 func IsNotDeletedT(tableName string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Where(tableName+".is_deleted = ?", false)
+	}
+}
+
+func IsNotDeletedAtT(tableName string) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Where(tableName + ".deleted_at IS NULL")
 	}
 }
 
