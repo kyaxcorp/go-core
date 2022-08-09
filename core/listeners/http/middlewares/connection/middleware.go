4e702e386048d5739ae35422a3a2dd606fc9cac2
Tabuci Octavian <octavian@tabuci.org>
2022-08-09 21:10:35 +0300

    http conn middleware fixes

--- core/listeners/http/middlewares/connection/middleware.go
+++ core/listeners/http/middlewares/connection/middleware.go
@@ -2,31 +2,11 @@ package connection
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/gookit/color"
 	"github.com/kyaxcorp/go-core/core/logger/model"
 )
 
-func GetMiddleware(logger *model.Logger) gin.HandlerFunc {
-	conn := GenerateConnDetails()
-	return conn.GetHandlerFunc(logger)
-}
-
-func (c *ConnDetails) GetHandlerFunc(logger *model.Logger) gin.HandlerFunc {
-	return func(gin *gin.Context) {
-
-		// Set the context to the struct!
-		c.C = gin
-		c.generateDetails()
-		// Debug the connection
-
-		logger.Logger.Info().
-			Str("host", c.Host).
-			Str("client_ip", c.ClientIPAddress).
-			Int("client_port", c.ClientPort).
-			Str("user_agent", c.UserAgent).
-			Str("remote_addr", c.RemoteAddr).
-			Str("request_path", c.RequestPath).
-			Str("referer", c.Referer).
-			Msg(color.Style{color.LightGreen, color.OpBold}.Render("new connection"))
+func Middleware(logger *model.Logger) gin.HandlerFunc {
+	return func(context *gin.Context) {
+		New().SetGinContext(context).SetLogger(logger).Process()
 	}
 }
