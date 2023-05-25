f73b0f232707f5396133e194255a2dcec70f5c24
Tabuci Octavian <octavian@tabuci.org>
2023-05-25 19:21:19 +0300

    db filter fixes

---
+++ core/clients/db/logger/runtime.go
@@ -0,0 +1,31 @@
+package logger
+
+import (
+	loggerConfig "github.com/kyaxcorp/go-core/core/logger/config"
+	"gorm.io/gorm"
+)
+
+// Create a runtime logger
+
+type Runtime struct {
+	InstanceName string
+	DBType       string
+	Level        int
+}
+
+func NewQuickConsole(db *gorm.DB, r Runtime) error {
+	level := 1
+	if r.Level > 0 {
+		level = r.Level
+	}
+
+	l := NewLogger(
+		r.InstanceName,
+		r.DBType,
+		loggerConfig.Config{
+			Level: level,
+		},
+	)
+	db.Logger = l
+	return nil
+}
